3142241dbb9f66d948a796fcadfb51a1b7d09478
vadlavaishnavii <vaishnavi.vadla06@gmail.com>
2026-02-15 13:43:04 +0530

    Create initial App component for Health Diet Assistant

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Health Diet Assistant</h1>
+      <p>Welcome to the Health Diet Assistant Web Application</p>
+    </div>
+  );
+}
+
+export default App;
