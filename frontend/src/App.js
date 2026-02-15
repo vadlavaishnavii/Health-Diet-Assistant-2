ae9eab6a09b8e8d90c0fa704a9ab15b487853da7
vadlavaishnavii <vaishnavi.vadla06@gmail.com>
2026-02-15 13:51:23 +0530

    connected Home page to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
+import Home from "./pages/Home";
+
 function App() {
   return (
     <div>
-      <h1>Health Diet Assistant</h1>
-      <p>Welcome to the Health Diet Assistant Web Application</p>
+      <Home />
     </div>
   );
 }
